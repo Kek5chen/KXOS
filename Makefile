eaa2c575b3b71e1369888bb1584843cbdb65c85e
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2022-12-20 16:19:07 +0100

    Initial commit

--- Makefile
+++ Makefile
@@ -1,16 +1,22 @@
-NAME = boot.iso
+ODIR = obj
+NAME = $(ODIR)/boot.img
 
 all: $(NAME)
 
 $(NAME): boot/boot.asm
-	nasm -f bin boot/boot.asm -o boot.img
-	dd if=boot.img of=$(NAME) bs=512 conv=sync
-	mkisofs -o $(NAME) -b boot.img
+# SETUP
+	mkdir -p $(ODIR)
+# ASSEMBLE
+	nasm -f bin boot/boot.asm -o $(ODIR)/boot.bin
+# COPY TO ISO FILE
+	dd if=/dev/zero of=$(NAME) bs=1024 count=20480
+	mkfs.ext3 -F -b 1024 $(NAME) 20480
+	dd if=$(ODIR)/boot.bin of=$(NAME) conv=notrunc
 
 clean:
-	rm -f boot.img $(NAME)
+	rm -f $(NAME)
 
 run: $(NAME)
-	qemu-system-x86_64.exe -cdrom $(NAME)
+	qemu-system-x86_64.exe -hda $(NAME)
 
 .PHONY: all clean run
--- boot/boot.asm
+++ boot/boot.asm
@@ -4,10 +4,22 @@
 ;============================================================
 
 bits 16									; boot into 16 bit real-mode.
-org 0									; start at 0x0
+org 0x7c00								; start at 0x7c00
 
-mov si, msg_booting
-call print_string
+										; Set background and foreground colour
+mov ah, 0x06							; Clear / scroll screen up function
+xor al, al								; Number of lines by which to scroll up (00h = clear entire window)
+mov bh, 0x30							; Background/foreground colour. (https://en.wikipedia.org/wiki/BIOS_color_attributes)
+xor cx, cx								; Row,column of window's upper left corner
+mov dx, 0x184f							; Row,column of window's lower right corner
+int 0x10								; Issue BIOS video services interrupt with function 0x06
+										; Set cursor position to 0,0
+mov ah, 0x02							; Set cursor position function
+mov bh, 0x00							; Page number (00h = active page)
+mov dh, 0x00							; Row number (00h = top row)
+mov dl, 0x00							; Column number (00h = leftmost column)
+int 0x10								; Issue BIOS video services interrupt with function 0x02
+
 jmp start								; jump to start
 
 ;============================================================
@@ -18,6 +30,8 @@ bytesPerSector	equ	512		 			; bytes per sector
 
 ; RUNTIME VALUES
 bsDriveNum		db	0	  				; drive number
+
+
 
 ;============================================================
 ; FUNCTION: printString
@@ -37,6 +51,9 @@ print_string:
 	print_string_end:
 		pop	si								; restore si
 		ret									; return
+;============================================================
+
+
 
 ;============================================================
 ; FUNCTION: start
@@ -52,7 +69,7 @@ start:
 	call print_string					; print string
 	cli									; disable interrupts
 										; set up registers
-	mov ax, 0x07C0						; set ax to 0x07C0
+	mov ax, 0x0							; set ax to 0x0
 	mov ds, ax							; set default data segment to ax
 	mov es, ax							; set extra segment to ax
 	mov fs, ax							; set extra segment to ax
@@ -63,7 +80,10 @@ start:
 	mov	sp, 0xFFFF						; set stack pointer to 0xFFFF
 	sti									; enable interrupts since we're done with setup
 	mov BYTE [bsDriveNum], dl			; store drive number in bsDrive
+	mov si, msg_kernel					; set si to msg_kernel
+	call print_string					; print string
 	jmp $								; infinite loop ( jump to current location )
+;============================================================
 
 
 
@@ -78,7 +98,7 @@ msg_kernel:
 	db "Loading kernel...", 0x0D, 0x0A, 0x00
 
 ;============================================================
-; END OF FILE
+; END OF CODE
 ;============================================================
 
 times 510-($-$$) db 0x00
