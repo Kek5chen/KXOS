7fc9ab6b41a40e3f8f07941287e3c8532abbc678
Kek5chen <kx@imkx.dev>
2023-09-25 06:30:20 +0200

    simpler kernel to get it to work

--- kernel/entry.c
+++ kernel/entry.c
@@ -7,7 +7,7 @@ void print_string(const char *s, unsigned char color, int pos);
 void print_character(char c, unsigned char color, int pos);
 void clear_screen();
 
-typedef struct telechar_s
+typedef struct __attribute__((packed)) telechar_s
 {
 	char c;
 	unsigned char a;
@@ -16,43 +16,10 @@ typedef struct telechar_s
 __attribute__((section(".text.kernel_entry")))
 int	kernel_entry(void)
 {
-	int i = MAKE_XY(3, 4);
-	char key;
-	char key_code;
-	char keys[128];
-	setup_keymap();
-	print_string("> ", 0x30, MAKE_XY(0, 4));
+	clear_screen();
+	print_string("Hello", 0x0A, MAKE_XY(0, 0));
 	while (1)
-	{
-		if (i >= MAKE_XY(80, 20)) {
-			clear_screen();
-			i = 0;
-		}
-		key = read_character();
-		if (!(key & 0xFF))
-			continue;
-		key_code = to_keycode(key);
-		if (!key_code)
-			continue;
-		if(keys[key] && key_code > 0)
-			continue;
-		if (!keys[key] && key_code < 0) {
-			keys[key] = 0;
-			continue;
-		}
-		keys[key] = 1;
-		if (key_code == '\b') {
-			if (i > 0) {
-				i--;
-				print_character(' ', 0x30, i);
-			}
-		} else if (key == '\n') {
-			i += 80 - i % 80;
-		} else {
-			print_character(key, 0x30, i);
-			i++;
-		}
-	}
+		;
 	return 0xABC;
 }
 
@@ -66,8 +33,7 @@ void clear_screen()
 
 void print_character(char c, unsigned char color, int pos)
 {
-	telechar_t *p = (telechar_t *)VMB;
-	p += pos;
+	telechar_t *p = (telechar_t *)VMB + pos;
 	p->c = c;
 	p->a = color;
 }
