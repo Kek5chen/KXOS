23ac590683b3caa5232098225422dd6aa98f841c
Kek5chen <kx@imkx.dev>
2023-09-26 00:54:32 +0200

    fix input

--- kernel/entry.c
+++ kernel/entry.c
@@ -21,14 +21,36 @@ int strlen(const char *s)
 	return i;
 }
 
+void print_number(int n, unsigned char color, int pos);
 __attribute__((section(".text.kernel_entry")))
 int	_start(void)
 {
 	clear_screen();
 	char *str = "Welcome to KXOS";
 	print_string(str, 0x0A, MAKE_XY(40 - strlen(str) / 2, 12));
-	while (1)
-		;
+
+	print_string("Scancode: ", 0x0A, MAKE_XY(34, 14));
+	print_string("Keycode: ", 0x0A, MAKE_XY(34, 15));
+	print_string("Character:", 0x0A, MAKE_XY(34, 16));
+	print_number(0, 0x0A, MAKE_XY(45, 14));
+	print_number(0, 0x0A, MAKE_XY(45, 15));
+	print_character(' ', 0x0A, MAKE_XY(45, 16));
+	while (1) {
+		unsigned char read = read_character_async();
+		if (read == 0)
+			continue;
+
+		if (read >= 128)
+			continue;
+
+		char keycode = to_keycode(read);
+		print_string("   ", 0x0A, MAKE_XY(45, 14));
+		print_string("   ", 0x0A, MAKE_XY(45, 15));
+		print_character(' ', 0x0A, MAKE_XY(45, 16));
+		print_number(read, 0x0A, MAKE_XY(45, 14));
+		print_number(keycode, 0x0A, MAKE_XY(45, 15));
+		print_character(keycode, 0x0A, MAKE_XY(45, 16));
+	}
 	return 0xABC;
 }
 
@@ -56,4 +78,28 @@ void print_string(const char *s, unsigned char color, int pos)
 		s++;
 		pos++;
 	}
+}
+
+// Function to recursively print the digits
+int print_number_recursive(int num, unsigned char col, int position) {
+	if (num == 0) return position;
+
+	position = print_number_recursive(num / 10, col, position);
+	print_character((num % 10) + '0', col, position);
+	return position + 1;
+}
+
+void print_number(int n, unsigned char color, int pos)
+{
+	if (n < 0) {
+		print_character('-', color, pos);
+		n = -n;
+		pos++;
+	}
+	if (n == 0) {
+		print_character('0', color, pos);
+		return;
+	}
+
+	print_number_recursive(n, color, pos);
 }
